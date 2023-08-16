7dd7468ef9640498ffbb6632e97117f42fe55f7e
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-08-16 13:39:02 -0400

    Create todoItem.ts

---
+++ src/todoItem.ts
@@ -0,0 +1,15 @@
+class todoItem {
+    title: string;
+    description: string;
+    dueDate: string;
+    priority: "low" | "medium" | "high" | "none";
+    notes: Array<string>;
+
+    constructor(title: string, description: string, dueDate: string, priority: "low" | "medium" | "high" | "none", notes: Array<string>) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+}
